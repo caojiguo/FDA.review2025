2fd3b0246fa21da6986c22eabb8dc38c967c9a01
mynanshan <muye.nanshan@gmail.com>
2025-04-02 14:33:21 +0800

    update sofnn

--- Scalar-on-Function Regression/Readme.md
+++ Scalar-on-Function Regression/Readme.md
@@ -2,5 +2,13 @@
 
 ### Files
 
-* `SoFNN.R`: Functions implement the scalar-on-function neural network
-* `sof-fnn.Rmd`: Rmarkdown demonstrating an application of the SoF NN on the tecator dataset.
+-   `SoFNN.R`: A function \`fit.sofnn\` is implemented to fit the scalar-on-function neural network.
+-   `sof-fnn.Rmd`: Rmarkdown demonstrating an application of the SoF NN on the tecator dataset.
+
+### Tecator Data
+
+![Tecator dataset](figures/tecator.jpg){width="432"}
+
+### Results
+
+![Model fitting results.](figures/sofnn-fit.jpg)
--- Scalar-on-Function Regression/demo-sof-fnn.R
+++ Scalar-on-Function Regression/demo-sof-fnn.R
@@ -31,12 +31,16 @@ sofnn = fit.sofnn(
   y, X, tgrid = tvals, lambda = 1e-7, nbasis = 11,
   hidden_sizes = c(16), act_func = "relu", max_epoch = 1000)
 
+# jpeg("figures/sofnn-fit.jpg", width = 640, height = 360)
 par(mfrow=c(1,2))
 predict(sofnn, X=X, tgrid=tvals) |> 
   plot(y, main="True vs. Predicted", xlab=expression(hat(y)), ylab="y")
 abline(coef=c(0,1), col=2)
 plot(sofnn$func_weights[[1]], xlab="t", ylab="", main="Functional Weights")
 par(mfrow=c(1,1))
+# dev.off()
+
+ggsave("figures/tecator.jpg", width = 5, height = 3.5)
 
 sofnn = fit.sofnn(
   y, X, tgrid = tvals, lambda = 1e3, nbasis = 11,
